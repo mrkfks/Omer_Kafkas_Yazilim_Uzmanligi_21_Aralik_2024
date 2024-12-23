356cbec5cff4bbbbe8e8ee850f2a21d6d62e07e9
mrkfks <mr.kfks@outlook.com>
2024-12-23 13:50:33 +0300

    mrkfks
    Ders 2

---
+++ 1_CSharp/1_Topics/2_Day/2_Day/Program.cs
@@ -0,0 +1,27 @@
+﻿// Kullanıcıdan veri alma
+
+Console.WriteLine("Name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Surname:");
+string surname  = Console.ReadLine();
+
+string nameSurname = name + " " +  surname;
+Console.WriteLine(nameSurname);
+
+// ******************************************************************************
+
+//Type casting - Tür Dönüşümü
+Console.WriteLine("Age:");
+string stAge  = Console.ReadLine();
+int age = Convert.ToInt32(stAge);
+Console.WriteLine(age);
+
+// ******************************************************************************
+
+double n1 = 50.6;
+double n2 = 70.6;
+double = n1 + n2; //121.2
+int cdSum = Convert.ToInt32(cdSum); //121
+
+int cN1 = Convert.ToInt32
