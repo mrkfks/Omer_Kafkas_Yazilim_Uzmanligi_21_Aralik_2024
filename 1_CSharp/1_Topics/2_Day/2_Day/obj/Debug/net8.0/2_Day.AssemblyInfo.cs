53d0d35b934a50671d89af9032433a4756f8ecea
mrkfks <mr.kfks@outlook.com>
2024-12-24 14:12:31 +0300

    mrkfks
    Day_2

--- 1_CSharp/1_Topics/2_Day/2_Day/obj/Debug/net8.0/2_Day.AssemblyInfo.cs
+++ 1_CSharp/1_Topics/2_Day/2_Day/obj/Debug/net8.0/2_Day.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2_Day")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85509fe4f558bfe89c6e23b15e6faa85c5f49d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356cbec5cff4bbbbe8e8ee850f2a21d6d62e07e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("2_Day")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2_Day")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
