dabdb2047b234392f53c95ac162ec87845a41683
mrkfks <mr.kfks@outlook.com>
2024-12-25 17:35:51 +0300

    mrkfks
    Homework_2

--- 1_CSharp/2_Homework/HomeWork_1/HomeWork_1/Program.cs
+++ 1_CSharp/2_Homework/HomeWork_1/HomeWork_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Homework-1
 
+//  ********************************************************************************************************************
 
 /*1. Kullanıcıdan adını, yaşını ve doğum yılını alarak, kullanıcının yaşını kontrol eden 
 ve doğru olup olmadığını ekrana yazdıran bir program yazın.*/
@@ -19,11 +20,10 @@ Console.WriteLine("Mevcut yıl:");
 string dateYear = Console.ReadLine();
 int dateyear_int = Convert.ToInt32(dateYear);
 
-int difference = dateyear_int - birtYear_int;
+if (older_int == dateyear_int - birtYear_int) { Console.WriteLine("Yaşınız doğru hesaplanmıştır."); }
+else { Console.WriteLine("Yaşınız yanlış hesaplanmıştır.");}
 
-bool status = birtYear_int == difference;
-Console.WriteLine(status);
-
+//  ********************************************************************************************************************
 
 /*2. Kullanıcıdan bir sayı alarak, bu sayının çift mi tek mi olduğunu kontrol eden ve 
 sonucu ekrana yazdıran bir program yazın.*/
@@ -31,12 +31,10 @@ sonucu ekrana yazdıran bir program yazın.*/
 Console.WriteLine("Çift veya tek olup olmadığını teyitlemek istediğin bir sayı giriniz:");
 string number = Console.ReadLine();
 int number_int = Convert.ToInt32(number);
+if (number_int % 2 == 0) { Console.WriteLine("Girilen sayı çifttir."); }
+else { Console.WriteLine("Girilen sayı tektir."); }
 
-int number_mod = number_int % 2;
-bool numberBool = number_mod == 0;
-Console.WriteLine(numberBool);
-
-
+//  ********************************************************************************************************************
 
 /*3. Kullanıcıdan iki sayı alarak, bu sayıların toplamını, farkını, çarpımını ve bölümünü 
 hesaplayan ve sonuçları ekrana yazdıran bir program yazın. Bölme işleminde, 
@@ -60,15 +58,10 @@ Console.WriteLine($"Sayıların farkı: {fark}");
 int carpim = (number1int * number2int);
 Console.WriteLine($"Sayıların Çarpımı: {carpim}");
 
-status = false;
-status = number2int == 0;
-Console.WriteLine("Bölen sayı sıfır olamaz!!");
+if (number2int == 0) { Console.WriteLine("Bölen sayı sıfır olamaz!!"); }
+else { int bolen = (number1int / number2int); Console.WriteLine($"Sayıların bölümü: {bolen}");}
 
-status = number2int != 0;
-int bolen = (number1int / number2int);
-Console.WriteLine($"Sayıların bölümü: {bolen}");
-
-
+//  ********************************************************************************************************************
 
 /*4. Kullanıcıdan br ondalık sayı alarak, bu sayıyı tam sayıya dönüştürüp, 
 dönüştürülmüş değer ve orjnal değer ekrana yazdıran br program yazın.*/
@@ -80,6 +73,7 @@ Console.WriteLine(sayıDouble);
 int sayıİnt =Convert.ToInt32(sayı);
 Console.WriteLine(sayıİnt);
 
+//  ********************************************************************************************************************
 
 /*5. Kullanıcıdan bir sayı alarak, bu sayının karesn ve küpünü hesaplayan ve 
 sonuçları ekrana yazdıran br program yazın.*/
@@ -91,6 +85,8 @@ int karesi = sayiInt * sayiInt;
 Console.WriteLine($"Karesi {karesi}");
 int kupu = karesi * sayiInt;
 Console.WriteLine($"Küpü: {kupu}");
+
+//  ********************************************************************************************************************
 
 /*6. Kullanıcıdan br metin alarak, bu metn büyük harfe çevren ve ekrana yazdıran bir 
 program yazın.*/
@@ -109,6 +105,7 @@ Console.WriteLine("Metnin Büyük hale Çevrilmiş Hali:");
 metinUpper = metin.ToUpperInvariant();
 Console.WriteLine(metinUpper);
 
+//  ********************************************************************************************************************
 
 /*7. Kullanıcıdan iki sayı alarak, bu sayıların büyüklük karşılaştırmasını yapan ve 
 büyük olan sayıyı ekrana yazdıran bir program yazın. Sayılar eşitse, eşit olduklarını 
@@ -128,8 +125,14 @@ else if (sayi1Int < sayiInt2) { Console.WriteLine("İkinci sayı birinci sayıda
 
 else { Console.WriteLine("İki sayı da birbirine eşittir."); }
 
+//  ********************************************************************************************************************
+
 /*8. Kullanıcıdan bir sayı alarak, bu sayının pozitif mi negatif mi olduğunu kontrol eden 
 ve sonucu ekrana yazdıran bir program yazın.*/
 
 Console.WriteLine("Kontrolü yapılacak sayıyı giriniz:");
+if (sayiInt > 0) { Console.WriteLine("Sayı pozitiftir."); }
+else if (sayiInt < 0) { Console.WriteLine("Sayı negatiftir."); }
+else { Console.WriteLine("Sayı sıfırdır."); }
 
+//  ********************************************************************************************************************
