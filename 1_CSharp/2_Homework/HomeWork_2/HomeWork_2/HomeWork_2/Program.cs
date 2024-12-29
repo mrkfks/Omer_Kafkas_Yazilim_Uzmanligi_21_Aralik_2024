587b6374ea6234969609030d816a24bb1e976c8f
Ömer KAFKAS <mr.kfks@outlook.com>
2024-12-29 16:55:26 +0300

    mrkfks
    update

---
+++ 1_CSharp/2_Homework/HomeWork_2/HomeWork_2/HomeWork_2/Program.cs
@@ -0,0 +1,124 @@
+﻿// HomeWok_2
+
+
+/********************************************************************************************************************/
+//1.Soru: Bir öğrencinin adını, yaşını ve not ortalamasını tutan değişkenler tanımlayın ve bu bilgileri ekrana yazdırın.
+//Cevap:
+
+
+
+
+
+
+/********************************************************************************************************************/
+//2. Soru: Bir dikdörtgenin uzunluğunu ve genişliğini kullanıcıdan alarak alanını hesaplayan bir program yazın.
+//Cevap:
+
+
+
+
+
+
+/********************************************************************************************************************/
+//3. Soru: Kullanıcıdan alınan bir string değer integer'a çevirip, bu değer 10 ile çarpan ve sonucu ekrana yazdıran bir program yazın.
+//Cevap:
+
+
+
+
+
+/********************************************************************************************************************/
+//4. Soru: Kullanıcıdan alınan bir double değeri integer'a çevirip, bu değer ekrana yazdıran bir program yazın.
+//Cevap:
+
+
+
+
+
+
+/********************************************************************************************************************/
+//5. Soru: Kullanıcıdan adını ve doğum yılını alarak, kullanıcının yaşını hesaplayan ve ekrana yazdıran bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//6. Soru: Kullanıcıdan iki sayı alarak bu sayıların toplamını, farkını, çarpımını ve bölümünü hesaplayan bir program yazın.
+//Cevap:
+
+
+
+
+
+
+/********************************************************************************************************************/
+//7. Soru: Kullanıcıdan alınan bir sayının pozitif, negatif veya sıfır olup olmadığını kontrol eden bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//8. Soru: Kullanıcıdan alınan bir not değerine göre, notun "Geçti" veya "Kaldı" şeklinde olup olmadığını belirleyen bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//9. Soru: 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//10. Soru: Kullanıcıdan alınan bir sayının faktöriyelin hesaplayan bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//11. Soru: Kullanıcıdan alınan k sayının her ikisinin de pozitif olup olmadığını kontrol eden bir program yazın.
+//Cevap:
+
+
+
+
+
+
+
+/********************************************************************************************************************/
+//12. Soru: Kullanıcıdan alınan bir sayının 10 ile 20 arasında olup olmadığını kontrol eden bir program yazın.
+//Cevap:
+
+
+
+
+
+
+/********************************************************************************************************************/
+
+
+
