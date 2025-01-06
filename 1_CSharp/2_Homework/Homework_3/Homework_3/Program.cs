fc1343ba118f8606663d60cfe181891e6f9de97d
mrkfks <mr.kfks@outlook.com>
2025-01-06 22:38:23 +0300

    mrkfks
    update

---
+++ 1_CSharp/2_Homework/Homework_3/Homework_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //1.Soru: Bir Hesap Makinesi sınıfı oluşturun.Bu sınıfta Topla, Çıkar, Çarp ve Böl fonksiyonları olsun.
+        //Bu fonksiyonları kullanarak iki sayının toplamını, farkını, çarpımını ve bölümünü hesaplayın.
+        //Cevap:
+
+        //2.Soru: Bir Dikdörtgen sınıfı oluşturun. Bu sınıfta Uzunluk ve Genişlik özellikleri ile Alan ve Çevre fonksiyonları olsun.
+        //Bu fonksiyonları kullanarak bir dikdörtgenin alanını ve çevresini hesaplayın.
+
+
+        //3.Soru: Bir Kare sınıfı oluşturun. Bu sınıfta Kenar özelliği le Alan ve Çevre fonksiyonları olsun.
+        //Bu fonksiyonları kullanarak bir karenin alanını ve çevresini hesaplayın.
+
+
+        //4.Soru: Bir Daire sınıfı oluşturun. Bu sınıfta Yarıçap özelliği ile Alan ve Çevre fonksiyonları olsun.
+        //Bu fonksiyonları kullanarak bir dairenin alanını ve çevresini hesaplayın.
+
+
+        //5.Soru: Bir Öğrenci sınıfı oluşturun. Bu sınıfta isim, yas ve notlar özellikleri le Not Ekle ve Ortalama Hesapla fonksiyonları olsun.
+        //Bu fonksiyonları kullanarak bir öğrencinin notlarını ekleyin ve not ortalamasını hesaplayın.
+
+
+        //6.Soru: Bir Faktöriyel fonksiyonu yazın.
+        //Bu fonksiyon, verilen bir sayının faktöriyelini hesaplasın. 
+
+
+        //7.Soru: Bir Fibonacci fonksiyonu yazın. Bu fonksiyon, verilen bir sayının Fibonacci dizsindeki değerin hesaplasın. (Fibonacci formülünü araştırınız.) 
+
+
+        //8.Soru: Bir Asal mı fonksiyonu yazın.Bu fonksiyon, verilen bir sayının asal olup olmadığını kontrol etsin. (Asal sayı formülünü araştırınız.)
+
+
+        //9.Soru: Bir En Büyük Ortak Bölen fonksiyonu yazın.Bu fonksiyon, verilen iki sayının en büyük ortak bölenini hesaplasın.
+
+
+        //10.Soru: Bir Küçükten Büyüğe Sırala fonksiyonu yazın. Bu fonksiyon, verilen bir dizi sayıyı küçükten büyüğe sıralasın.
+
+
+
+        }
+    }
+}
