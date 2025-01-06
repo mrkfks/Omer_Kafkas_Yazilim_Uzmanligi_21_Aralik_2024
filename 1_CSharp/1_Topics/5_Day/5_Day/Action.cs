f6099664a22edeafc377081452263562ced8944b
mrkfks <mr.kfks@outlook.com>
2025-01-06 13:53:01 +0300

    mrkfks
    update

---
+++ 1_CSharp/1_Topics/5_Day/5_Day/Action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Day
+{
+    public class Action
+    {
+
+        public int call(int number)
+        {
+            int end = number * number;
+            return end;
+        }
+
+    }
+}
