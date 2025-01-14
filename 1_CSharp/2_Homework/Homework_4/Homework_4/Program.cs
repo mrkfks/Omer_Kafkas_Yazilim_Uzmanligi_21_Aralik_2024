55335582e61b3ea39f42d0ad0b46ab9796ca370a
mrkfks <mr.kfks@outlook.com>
2025-01-14 16:00:48 +0300

    mrkfks
    update

---
+++ 1_CSharp/2_Homework/Homework_4/Homework_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Soru1: (Array) Bir dizi oluşturun ve bu dizideki elemanların ortalamasını hesaplayan bir program yazın.
+
+        //Soru2: (If) Kullanıcıdan bir sayı alın ve bu sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
+
+        //Soru3: (Else - If) Kullanıcıdan bir not değeri alın ve bu değere göre "Geçti", "Kaldı" veya "Bütünlemeye Kaldı" gibi mesajlar veren bir program yazın.
+
+        //Soru4: (For) 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yazın.
+
+        //Soru5: (Foreach) Bir string dizisi oluşturun ve bu dizideki her bir kelimeyi ekrana yazdıran bir program yazın.
+
+        //Soru6: (Nesne Üretimi) Bir "Araba" sınıfı oluşturun ve bu sınıftan birkaç nesne üretip içerisindeki bir metottan geriye dönen string ifadeyi ekrana yazdıran bir program yazın.
+
+        //Soru7: (Fonksiyonlar) İki sayıyı toplayan bir fonksiyon yazın ve bu fonksiyonu kullanarak kullanıcıdan alınan iki sayının toplamını ekrana yazdıran bir program yazın.
+
+        //Soru8: (Tür Dönüşümü) Kullanıcıdan bir string olarak alınan sayıyı integer türüne dönüştürüp karesini hesaplayan bir program yazın.
+
+        //Soru9: (Console.ReadLine) Kullanıcıdan adını ve yaşını alıp ekrana "Merhaba [Ad], [Yaş] yaşındasınız." şeklinde yazdıran bir program yazın.
+
+        //Soru10: (Kombinasyon) Bir dizi oluşturun, bu dizideki elemanları sıralayın ve sıralanmış diziyi ekrana yazdıran bir program yazın.
+        }
+    }
+}
