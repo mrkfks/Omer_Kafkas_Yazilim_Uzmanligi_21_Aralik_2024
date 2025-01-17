c913cee8a4b670c2b9533f3b7360276cc41bb8f1
mrkfks <mr.kfks@outlook.com>
2025-01-17 21:47:33 +0300

    mrkfks
    update

---
+++ 1_CSharp/2_Homework/Homework_4/Homework_4/Action.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class Action
+    {
+    public void OrtalamaDizi(int[] )  
+    }
+}
--- 1_CSharp/2_Homework/Homework_4/Homework_4/Program.cs
+++ 1_CSharp/2_Homework/Homework_4/Homework_4/Program.cs
@@ -11,25 +11,35 @@ namespace Homework_4
     {
         static void Main(string[] args)
         {
-        //Soru1: (Array) Bir dizi oluşturun ve bu dizideki elemanların ortalamasını hesaplayan bir program yazın.
+            //Soru1: (Array) Bir dizi oluşturun ve bu dizideki elemanların ortalamasını hesaplayan bir program yazın.
+            //cevap:
 
-        //Soru2: (If) Kullanıcıdan bir sayı alın ve bu sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
+            Console.WriteLine("Ortalaması alınacak dizini boyutunu giriniz:");
+            int ArrayLenght = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[ArrayLenght];
+            for (int i = 0; i < ArrayLenght; i++)
+            {
+                Console.WriteLine("Dizinin " + (i + 1) + ". elemanını giriniz:");
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-        //Soru3: (Else - If) Kullanıcıdan bir not değeri alın ve bu değere göre "Geçti", "Kaldı" veya "Bütünlemeye Kaldı" gibi mesajlar veren bir program yazın.
+            //Soru2: (If) Kullanıcıdan bir sayı alın ve bu sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
 
-        //Soru4: (For) 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yazın.
+            //Soru3: (Else - If) Kullanıcıdan bir not değeri alın ve bu değere göre "Geçti", "Kaldı" veya "Bütünlemeye Kaldı" gibi mesajlar veren bir program yazın.
 
-        //Soru5: (Foreach) Bir string dizisi oluşturun ve bu dizideki her bir kelimeyi ekrana yazdıran bir program yazın.
+            //Soru4: (For) 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yazın.
 
-        //Soru6: (Nesne Üretimi) Bir "Araba" sınıfı oluşturun ve bu sınıftan birkaç nesne üretip içerisindeki bir metottan geriye dönen string ifadeyi ekrana yazdıran bir program yazın.
+            //Soru5: (Foreach) Bir string dizisi oluşturun ve bu dizideki her bir kelimeyi ekrana yazdıran bir program yazın.
 
-        //Soru7: (Fonksiyonlar) İki sayıyı toplayan bir fonksiyon yazın ve bu fonksiyonu kullanarak kullanıcıdan alınan iki sayının toplamını ekrana yazdıran bir program yazın.
+            //Soru6: (Nesne Üretimi) Bir "Araba" sınıfı oluşturun ve bu sınıftan birkaç nesne üretip içerisindeki bir metottan geriye dönen string ifadeyi ekrana yazdıran bir program yazın.
 
-        //Soru8: (Tür Dönüşümü) Kullanıcıdan bir string olarak alınan sayıyı integer türüne dönüştürüp karesini hesaplayan bir program yazın.
+            //Soru7: (Fonksiyonlar) İki sayıyı toplayan bir fonksiyon yazın ve bu fonksiyonu kullanarak kullanıcıdan alınan iki sayının toplamını ekrana yazdıran bir program yazın.
 
-        //Soru9: (Console.ReadLine) Kullanıcıdan adını ve yaşını alıp ekrana "Merhaba [Ad], [Yaş] yaşındasınız." şeklinde yazdıran bir program yazın.
+            //Soru8: (Tür Dönüşümü) Kullanıcıdan bir string olarak alınan sayıyı integer türüne dönüştürüp karesini hesaplayan bir program yazın.
 
-        //Soru10: (Kombinasyon) Bir dizi oluşturun, bu dizideki elemanları sıralayın ve sıralanmış diziyi ekrana yazdıran bir program yazın.
+            //Soru9: (Console.ReadLine) Kullanıcıdan adını ve yaşını alıp ekrana "Merhaba [Ad], [Yaş] yaşındasınız." şeklinde yazdıran bir program yazın.
+
+            //Soru10: (Kombinasyon)  Bir dizi oluşturun, bu dizideki elemanları sıralayın ve sıralanmış diziyi ekrana yazdıran bir program yazın.
         }
     }
 }
