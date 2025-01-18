73f3381a5e3c540e6177cbb8bee07aa8762003ac
mrkfks <mr.kfks@outlook.com>
2025-01-18 22:47:47 +0300

    mrkfks
    update

--- 1_CSharp/2_Homework/Homework_4/Homework_4/Action.cs
+++ 1_CSharp/2_Homework/Homework_4/Homework_4/Action.cs
@@ -8,12 +8,33 @@ namespace Homework_4
 {
     public class Action
     {
-    public void OrtalamaDizi(int[] Array)
+        public void OrtalamaDizi(int[] Array)
         {
-           int ArraySum = Array.Sum();
+            int ArraySum = Array.Sum();
             Console.WriteLine($"Dizinin elemanlarının toplamı: {ArraySum}");
-            double ArrayAvg =  Array.Average();
+            double ArrayAvg = Array.Average();
             Console.WriteLine($"Dizinin elemanlarının ortalaması: {ArrayAvg}");
         }
+        public void SayiDurum(int sayi)
+        {
+            if (sayi > 0) { Console.WriteLine("Sayı Pozitiftir."); }
+            else if (sayi < 0) { Console.WriteLine("Sayi negatiftir."); }
+            else { Console.WriteLine("Sayı Sıfırdır."); }
+        }
+        public void PointStatus(int point)
+        {
+            if (point <= 100 && point >= 90) { Console.WriteLine("Harf Notu: A"); }
+            else if (point < 90 && point >= 80) { Console.WriteLine("Harf Notu: B"); }
+            else if (point < 80 && point >= 70) { Console.WriteLine("Harf Notu: C"); }
+            else if (point < 70 && point >= 60) { Console.WriteLine("Harf Notu: D"); }
+            else if (point < 60 && point >= 0) { Console.WriteLine("Harf Notu: F"); }
+            else { Console.WriteLine("Geçersiz Puan."); }
+        }
+        public void ForLoop(int basSay , int bitSay ) 
+        {
+            for (int i = basSay; i < (bitSay + 1); i++) { Console.WriteLine(i); }
+        }
+
+        
     }
 }
--- 1_CSharp/2_Homework/Homework_4/Homework_4/Program.cs
+++ 1_CSharp/2_Homework/Homework_4/Homework_4/Program.cs
@@ -29,24 +29,53 @@ namespace Homework_4
 
 
             //Soru2: (If) Kullanıcıdan bir sayı alın ve bu sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
+            //cevap:
 
+            Console.WriteLine("Değerlendirilecek sayıyı giriniz:");
+            int sayiInt = Convert.ToInt32(Console.ReadLine());
+            Action sayiAction = new Action();
+            action.SayiDurum(sayiInt);
 
 
             //Soru3: (Else - If) Kullanıcıdan bir not değeri alın ve bu değere göre "Geçti", "Kaldı" veya "Bütünlemeye Kaldı" gibi mesajlar veren bir program yazın.
+            //cevap:
+
+            Console.WriteLine("Değerlendirilmesini istediğiniz not değerini giriniz:");
+            int pointInt = Convert.ToInt32(Console.ReadLine());
+            Action pointAction = new Action();
+            action.PointStatus(pointInt);
+
 
             //Soru4: (For) 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yazın.
+            //cevap:
+
+            Action forLoopStatus = new Action();
+            action.ForLoop(1, 100);
 
             //Soru5: (Foreach) Bir string dizisi oluşturun ve bu dizideki her bir kelimeyi ekrana yazdıran bir program yazın.
+            //cevap:
+
+            string[] kelimeler = { "Merhaba", "Dünya", "C#", "Programlama" };
+            foreach (string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
 
             //Soru6: (Nesne Üretimi) Bir "Araba" sınıfı oluşturun ve bu sınıftan birkaç nesne üretip içerisindeki bir metottan geriye dönen string ifadeyi ekrana yazdıran bir program yazın.
+            //cevap:
 
             //Soru7: (Fonksiyonlar) İki sayıyı toplayan bir fonksiyon yazın ve bu fonksiyonu kullanarak kullanıcıdan alınan iki sayının toplamını ekrana yazdıran bir program yazın.
+            //cevap:
+
 
             //Soru8: (Tür Dönüşümü) Kullanıcıdan bir string olarak alınan sayıyı integer türüne dönüştürüp karesini hesaplayan bir program yazın.
+            //cevap:
 
             //Soru9: (Console.ReadLine) Kullanıcıdan adını ve yaşını alıp ekrana "Merhaba [Ad], [Yaş] yaşındasınız." şeklinde yazdıran bir program yazın.
+            //cevap:
 
             //Soru10: (Kombinasyon)  Bir dizi oluşturun, bu dizideki elemanları sıralayın ve sıralanmış diziyi ekrana yazdıran bir program yazın.
+            //cevap:
         }
     }
 }
