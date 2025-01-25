a4185bb291b94a16e7ed778b00f627349612caf1
mrkfks <mr.kfks@outlook.com>
2025-01-25 21:04:20 +0300

    mrkfks
    update

---
+++ 1_CSharp/2_Homework/Homework_5/Homework_5/Actions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    public class Actions
+    {
+        public void stringConcat()
+        { 
+
+        
+        }
+        public void stringEquals()
+        {
+
+        }
+        public void stringContains()
+        {
+
+        }
+        public void stringSubstring()
+        {
+
+        }
+        public void stringInsert()
+        {
+
+        }
+        public void stringRemove()
+        {
+
+        }
+        public void stringSplit()
+        {
+
+        }
+        public void stringReplace()
+        {
+
+        }
+        public void stringTrim()
+        {
+
+        }
+        public void stringIndexOf()
+        {
+
+        }
+        public void stringIndexOf()
+        {
+
+        }
+    }
+}
