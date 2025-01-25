d32a5f1a2ea210bd91d65c982d2bc7745a4c7e6a
mrkfks <mr.kfks@outlook.com>
2025-01-25 18:59:20 +0300

    mrkfks
    update

---
+++ 1_CSharp/1_Topics/10_Day/10_Day/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Day
+{
+    public class Customer
+    {
+        public void Login(string email, string password)
+        {
+            Console.WriteLine($" {email} -  {password} ");
+        }
+        public void ProfileImageCrop(int width, int height)
+        {
+            try
+            { 
+            Console.WriteLine($" {width} -  {height} ");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ 1_CSharp/1_Topics/10_Day/10_Day/Profile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Day
+{
+    internal class Profile
+    {
+        //kurucu metotlar 
+        //Bir sınıf kurulurken sınıf içine parametre göndermeye yarar. 
+
+        //Kurallar
+        //Sınıf adı ile aynı isme sahip olmalıdır.
+        //void - return anahtar kelimesi alamazlar.
+        //Yazılmadığında default kurucu metot çalışmış olur.
+        //yazıldığında default kurucu metot çalışmaz. artık bizim yazdığımız geçerlidir.
+
+        string name = "";
+        string surname = "";
+
+        public Profile() 
+        {
+        
+        }
+
+        public Profile(string name)
+        {
+            this.name = name;
+        }
+
+        public Profile(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void call()
+        {
+            if (!name.Equals(""))
+            { 
+                Console.WriteLine(name);
+            }
+            if (!surname.Equals(""))
+            {
+                Console.WriteLine(surname);
+            }
+        }
+
+
+        public string ProfileName() 
+        {
+            return "Ali Bilmem";
+        }
+        
+    }
+}
