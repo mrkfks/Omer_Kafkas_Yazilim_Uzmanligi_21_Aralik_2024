e9ab7302d2a94c2ad285698bc7ad62a6f4a764d1
mrkfks <mr.kfks@outlook.com>
2025-02-03 13:21:02 +0300

    mrkfks
    update

--- 1_CSharp/1_Topics/12_Day/12_Day/inheritance/A.cs
+++ 1_CSharp/1_Topics/12_Day/12_Day/inheritance/A.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace _12_Day.inheritance
 {
-    internal class A
+    public class A : Base
     {
+        public int age = 30;
+
+        public override void Call()
+        { 
+            Console.WriteLine("A Write Call");
+        }
+
+        public int sum(int a, int b)
+        { 
+            int sm = a + b;
+            Console.WriteLine(sm);
+            return sm;
+        }
     }
 }
