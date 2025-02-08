3f63bc54666e1734c4c0106c628fdcd06e91b948
mrkfks <mr.kfks@outlook.com>
2025-02-08 14:13:26 +0300

    update

---
+++ 1_CSharp/1_Topics/14_Day/14_Day/user/IUser.cs
@@ -0,0 +1,17 @@
+﻿using _14_Day.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Day.user
+{
+    public interface IUser
+    {
+       bool UserRegister(UserModel userModel);
+       UserModel UserLogin(string email, string password);
+    }
+
+
+}
