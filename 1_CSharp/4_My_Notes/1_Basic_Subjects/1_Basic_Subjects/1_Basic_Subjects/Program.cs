43c83f8ae8cc8becfc9ff6987c82d751360ba750
mrkfks <mr.kfks@outlook.com>
2025-02-17 22:48:41 +0300

    upt

---
+++ 1_CSharp/4_My_Notes/1_Basic_Subjects/1_Basic_Subjects/1_Basic_Subjects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1_Basic_Subjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PritMessage();
+        }
+
+        public static void PritMessage()
+        {
+            Console.WriteLine("Bu Mesaj Bir Değer Döndürmez");
+        }
+
+        class MyClassPrivate 
+        {
+            private int MyPrivateVariable;
+        }
+
+        class MyClassİnternal
+        {
+            internal int MyInternalVariable;
+        }
+        class MyClassProtected
+        {
+            protected int MyProtectedVariable;
+        }
+        class MyClassProtectedInternal
+        {
+            protected internal int MyProtectedInternalVariable;
+        }
+        class MyClassPublic
+        {
+            public int MyPublicVariable;
+        }
+
+    }
+}
+
