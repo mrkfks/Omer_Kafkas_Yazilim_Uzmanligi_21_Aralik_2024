f559fcf953e0818ec776c3e968ad23c39e3cc994
mrkfks <mr.kfks@outlook.com>
2025-02-24 17:05:04 +0300

    Ömer Kafkas

---
+++ 1_CSharp/1_Topics/18_Day/18_Day/Calls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Day
+{
+    public class Calls
+    {
+        public Calls()
+        {
+            Console.WriteLine("line - 1");
+        }
+
+        public void Call1()
+        {
+            Console.WriteLine("Call1 Call");
+        }
+        public void Call2()
+        {
+            Console.WriteLine("Call2 Call");
+        }
+    }
+}
