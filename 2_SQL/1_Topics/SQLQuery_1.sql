ea1814a029297a8bd301a934bd09fa4609185ded
mrkfks <mr.kfks@outlook.com>
2025-03-08 19:17:28 +0300

    update

---
+++ 2_SQL/1_Topics/SQLQuery_1.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+users ([name], [surname], [email], [phone], [password],[savedate], [status])
+VALUES
+(
+    'Erkan',
+    'Bilmem',
+    'erkan@mail.com',
+    5554443322,
+    '12345',
+    '2025-03-08 09:27:12'
+    1
+)
