bef47abf1b704c861a408ad37ba3834a2a9d58d3
mrkfks <mr.kfks@outlook.com>
2025-06-24 23:14:51 +0300

    fe çalışması

--- 0_Diger/1_Kisisel_Calismalar/Deneme Frontend Çalışması/js/site.js
+++ 0_Diger/1_Kisisel_Calismalar/Deneme Frontend Çalışması/js/site.js
@@ -1,23 +1,14 @@
-document.getElementById("searchInput").addEventListener("keyup", function () {
-  const query = this.value;
-
-  if (query.length < 2) return; // çok kısa sorguları atla
-
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    if (this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      const list = document.getElementById("results");
-      list.innerHTML = "";
-
-      data.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item.title;
-        list.appendChild(li);
-      });
-    }
-  };
-
-  xhttp.open("GET", `https://newsapi.org/v2/everything?q=tesla&from=2025-05-17&sortBy=publishedAt&apiKey=f47028e02a4c43f789fcfb764b5cc113=${query}`, true);
-  xhttp.send();
-});
+$(document).ready(function() {
+  $('button').click(function() {
+    alert('buton aktif');
+  });
+  $('#submit').click(function() {
+    alert('Button Call Submit');
+  });
+  $('.form-label').click(function(){
+    alert('Form label click');
+  });
+  $('div.bg-danger').click(function(){
+    alert('dive tıklandı');
+  });
+}) 
