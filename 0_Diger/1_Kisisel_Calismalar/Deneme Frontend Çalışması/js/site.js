abe14de76c8193238f94329e56292a7d0ba1782d
mrkfks <mr.kfks@outlook.com>
2025-06-25 00:52:16 +0300

    upt

--- 0_Diger/1_Kisisel_Calismalar/Deneme Frontend Çalışması/js/site.js
+++ 0_Diger/1_Kisisel_Calismalar/Deneme Frontend Çalışması/js/site.js
@@ -11,4 +11,26 @@ $(document).ready(function() {
   $('div.bg-danger').click(function(){
     alert('dive tıklandı');
   });
+  $('div#box').click(function(){
+    $(this).toggleClass('bg-danger')
+  })
+  
+  $('#loginForm').submit(function (e) { 
+    e.preventDefault();
+    const email = $('#email').val()
+    const password = $('#password').val()
+    const remember =$('#remember').is(':checked');
+    const city = $('#city').val()
+    console.log(email, password, remember, city)
+    
+  });
+
+  $('#email').change(function (e) { 
+    console.log('değişti')
+  });
+
+  $('#email').keyup(function (e) { 
+    console.log('keyup call')
+  });
+
 }) 
