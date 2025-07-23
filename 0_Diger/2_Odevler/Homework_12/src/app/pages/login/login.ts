c8c6f3aa95e9bc5368a14a8fb30e74f3032b470c
mrkfks <mr.kfks@outlook.com>
2025-07-23 17:42:53 +0300

    update

--- 0_Diger/2_Odevler/Homework_12/src/app/pages/login/login.ts
+++ 0_Diger/2_Odevler/Homework_12/src/app/pages/login/login.ts
@@ -1,15 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { Router } from 'express';
+import { Router } from '@angular/router';
+import { emailValid } from '../../utils/valids';
 
 @Component({
   selector: 'app-login',
   imports: [RouterModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class Login {
+email = ''
+
+password = ''
+
+remember = false
+
+error = ''
+
+
 
 @ViewChild("emailRef")
 emailRef:ElementRef | undefined
@@ -17,12 +28,18 @@ emailRef:ElementRef | undefined
 @ViewChild("passwordRef")
 passwordRef: ElementRef | undefined
   
+userLogin(){
+  this.error = ''
+  const emailStatus = emailValid(this.email)
+  if(!emailStatus) {
+    this.error = 'Email Format Error'
+    this.emailRef!.nativeElement.focus()
+  }else if(this.password == ''){
+    this.error = 'Password Empty!'
+    this.passwordRef!.nativeElement.focus()
+  }
+}
 
-email = ''
-
-password = ''
-
-remember = false
 
 
 }
