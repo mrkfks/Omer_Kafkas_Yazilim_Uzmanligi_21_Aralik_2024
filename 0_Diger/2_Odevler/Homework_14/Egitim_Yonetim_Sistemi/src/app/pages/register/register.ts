1b28531f150bae8ec569961e0938692be1cac16e
mrkfks <mr.kfks@outlook.com>
2025-08-31 22:47:01 +0300

    upt

--- 0_Diger/2_Odevler/Homework_14/Egitim_Yonetim_Sistemi/src/app/pages/register/register.ts
+++ 0_Diger/2_Odevler/Homework_14/Egitim_Yonetim_Sistemi/src/app/pages/register/register.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BackgroundItem } from "../../components/background-item/background-item";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [BackgroundItem, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, BackgroundItem],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Register {
-form: any;
+private fb = inject(FormBuilder)
+
+readonly form = this.fb.group({
+  name: ['', [Validators.required, Validators.minLength(1)]],
+  surname: ['', [Validators.required, Validators.minLength(1)]],
+  email: ['', [Validators.required, Validators.email]],
+  password: ['', [Validators.required]]
+  
+})
 
 }
