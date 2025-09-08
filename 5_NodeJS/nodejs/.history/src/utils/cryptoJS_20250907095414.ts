2728bcd26d46a72b9031f695f857ba39ebc3bbff
mrkfks <mr.kfks@outlook.com>
2025-09-08 18:35:18 +0300

    upt

--- 5_NodeJS/nodejs/.history/src/utils/cryptoJS_20250907095414.ts
+++
@@ -1,6 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export const encrypt = (plainText: string) => {
-    const ciphertext = CryptoJS.AES.encrypt(plainText, 'key123').toString();
-    return ciphertext
-}
